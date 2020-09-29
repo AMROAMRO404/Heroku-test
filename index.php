1b4a7855bccda6d409e561a902714d79abc80f64
AMROAMRO404 <AMROAMRO404@github.com>
2020-09-29 23:07:31 +0300

    hhh

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php /*Do some PHP calculation or something*/ ?>
+     <table>
+     <h1>hhhh</h1>
+         <tr>
+             <td>Name</td>
+             <td><?php echo $name;?></td>
+         </tr>
+     </table>
